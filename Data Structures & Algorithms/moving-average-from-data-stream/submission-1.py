e54a7fae88a8a40442ceb2d2fe92f000274cf772
Dhawal Kumar Singh <dhawsingh@microsoft.com>
2026-05-20 23:13:06 +0530

    Reset: Clear all submissions to start fresh

--- Data Structures & Algorithms/moving-average-from-data-stream/submission-1.py
+++
@@ -1,9 +0,0 @@
-class MovingAverage:
-
-    def __init__(self, size: int):
-        self.window = []
-        self.size = size
-
-    def next(self, val: int) -> float:
-        self.window.append(val)
-        return sum(self.window[-self.size:]) / min(len(self.window),self.size)
--- Data Structures & Algorithms/two-integer-sum/submission-0.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hm = {}
-        for i in range(len(nums)):
-            val = nums[i]
-            if target - val in hm:
-                return [hm[target - val],i]
-            hm[val] = i
-        return []
